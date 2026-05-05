991a5ef6a357257811ea96c926985479f0fa5ed9
Sahenul Islam <sahenul.islam@anwargroup.com>
2026-05-05 16:58:08 +0600

    outbox add

---
+++ transactional-service/src/main/java/com/example/transactional_service/outbox/Outbox.java
@@ -0,0 +1,28 @@
+package com.example.transactional_service.outbox;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Outbox {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String eventType;
+
+    @Column(columnDefinition = "TEXT")
+    private String payload;
+
+    private String status;
+
+    private Instant createdAt;
+}
---
+++ transactional-service/src/main/java/com/example/transactional_service/outbox/OutboxService.java
@@ -0,0 +1,11 @@
+package com.example.transactional_service.outbox;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OutboxService {
+
+}
